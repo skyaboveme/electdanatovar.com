0bde0500f5c1c1e3334c87d3a629c79e481496d3
Steve Gibson <befoundonai@Steves-MacBook-Pro.local>
2026-04-06 08:47:21 -0500

    Update population count to 4200, add sitemap/robots, update seo tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://electdanatovar.com',
+  integrations: [sitemap()],
   adapter: cloudflare()
 });
